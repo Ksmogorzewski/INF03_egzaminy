7084c80fae9ecc9a0a1ff2316e65a816ce6ce9ba
Krzysztof Smogorzewski <krzysztofsmogorzewski50@gmail.com>
2023-05-22 17:40:51 +0200

    minor changes

---
+++ 2022/INF.03-01-22.06-SG/rozwiązanie/wedkuj.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <div class="main">
+        <div class="header">
+            <h1>Portal dla wędkarzy</h1>
+        </div>
+        <div class="middle">
+            <div class="left">
+                <div class="left1">
+                    <h3>Ryby zamieszkujące rzeki</h3>
+                    <ol>
+                        <li></li>
+                        <li></li>
+                    </ol>
+                </div>
+                <div class="left2">
+                    <h3>Ryby drapieżne naszych wód</h3>
+                    <table>
+                        
+                    </table>
+                </div>
+            </div>
+            <div class="right">
+                <img src="ryba1.jpg" alt="Sum">
+                <a href="kwerendy.txt">Pobierz kwerendy</a>
+            </div>
+        </div>
+        <div class="footer">
+            <p>Stronę wykonał: 00000000000</p>
+        </div>
+    </div>
+</body>
+</html>
